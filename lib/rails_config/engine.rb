62a8a6e20054d4531fb45cb2902ba342723cef4c
Eugen Kuksa <eugenk@informatik.uni-bremen.de>
2015-03-17 08:37:02 +0100

    Revert "Only define Engine if in Rails."
    This reverts commit 563e957a5764de897b6f2cfd783bcc0510f73378.

--- lib/rails_config/engine.rb
+++ lib/rails_config/engine.rb
@@ -1,7 +1,5 @@
 module RailsConfig
-  if defined?(::Rails::Engine)
-    class Engine < ::Rails::Engine
-      isolate_namespace RailsConfig
-    end
+  class Engine < ::Rails::Engine
+    isolate_namespace RailsConfig
   end
 end
